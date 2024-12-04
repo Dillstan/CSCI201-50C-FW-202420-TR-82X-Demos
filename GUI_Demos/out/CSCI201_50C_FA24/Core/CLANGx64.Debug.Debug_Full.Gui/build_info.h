b2d8fbee4effd595003dc0f74761c538ee5d7d45
avenable11 <avenable11@ivytech.edu>
2024-12-04 18:43:44 -0500

    first GUI program

---
+++ GUI_Demos/out/CSCI201_50C_FA24/Core/CLANGx64.Debug.Debug_Full.Gui/build_info.h
@@ -0,0 +1,9 @@
+#define bmYEAR   2024
+#define bmMONTH  12
+#define bmDAY    4
+#define bmHOUR   18
+#define bmMINUTE 41
+#define bmSECOND 36
+#define bmTIME   Time(2024, 12, 4, 18, 41, 36)
+#define bmMACHINE "3000-CC2312-22"
+#define bmUSER    "avenable"
---
+++ GUI_Demos/out/CSCI201_50C_FA24/HelloWorld1/CLANGx64.Debug.Debug_Full.Gui.Main/build_info.h
@@ -0,0 +1,11 @@
+#define bmYEAR   2024
+#define bmMONTH  12
+#define bmDAY    4
+#define bmHOUR   18
+#define bmMINUTE 41
+#define bmSECOND 41
+#define bmTIME   Time(2024, 12, 4, 18, 41, 41)
+#define bmMACHINE "3000-CC2312-22"
+#define bmUSER    "avenable"
+#define bmGIT_REVCOUNT "29"
+#define bmGIT_HASH "f3433b46f47f8cc9e013564a788723a5c9816267"
